254364c86b269159a7731a764ae68978fef74f01
Bruno Santos <bsilva89@hotmail.com>
2021-09-23 16:43:27 -0300

    init meal app

---
+++ meal_app/lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+import './category_meal_screen.dart';
+import './categories_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'MyMeal',
+      theme: ThemeData(
+        primarySwatch: Colors.pink,
+        accentColor: Colors.amber,
+        canvasColor: Color.fromRGBO(255, 254, 229, 1),
+        fontFamily: 'Raleway',
+        textTheme: ThemeData.light().textTheme.copyWith(
+              bodyText2: TextStyle(
+                color: Color.fromRGBO(20, 51, 51, 1),
+              ),
+              headline6: TextStyle(
+                fontFamily: 'RobotoCondensed',
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+      ),
+      home: CategoriesScreen(),
+      routes: {CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen()},
+    );
+  }
+}
