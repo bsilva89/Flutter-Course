68ad44ddb1e3730367a9a48d6c8934ee71e03be6
Bruno Santos <bsilva89@hotmail.com>
2021-09-28 10:24:32 -0300

    checkpoint shop_app

--- app_shop/lib/screens/products_overview_screen.dart
+++ app_shop/lib/screens/products_overview_screen.dart
@@ -1,10 +1,33 @@
 import 'package:flutter/material.dart';
 
-class ProductsOverview extends StatelessWidget {
-  const ProductdOverview({Key? key}) : super(key: key);
+import '../data/product_data.dart';
 
+import '../widgets/product_item.dart';
+
+class ProductsOverview extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'MyShop',
+        ),
+      ),
+      body: GridView.builder(
+        padding: const EdgeInsets.all(10),
+        itemCount: loadedProducts.length,
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          crossAxisSpacing: 10,
+          childAspectRatio: 3 / 2,
+          mainAxisSpacing: 10,
+        ),
+        itemBuilder: (ctx, i) => ProductItem(
+          loadedProducts[i].id,
+          loadedProducts[i].title,
+          loadedProducts[i].imageUrl,
+        ),
+      ),
+    );
   }
 }
