8e27ad3cf3063e8f902cb5006cc30666108228c0
Bruno Santos <bsilva89@hotmail.com>
2022-05-24 07:55:33 -0300

    start forms section and update dart version

--- app_shop/lib/providers/cart.dart
+++ app_shop/lib/providers/cart.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 import 'package:flutter/foundation.dart';
 
 class CartItem {
@@ -65,6 +67,26 @@ class Cart with ChangeNotifier {
 
   void removeItem(String productId) {
     _items.remove(productId);
+    notifyListeners();
+  }
+
+  void removeSingleItem(String productId) {
+    if (!_items.containsKey(productId)) {
+      return;
+    }
+
+    if (_items[productId]!.quantity > 1) {
+      _items.update(
+        productId,
+        (value) => CartItem(
+            id: value.id,
+            title: value.title,
+            quantity: value.quantity - 1,
+            price: value.price),
+      );
+    } else {
+      _items.remove(productId);
+    }
     notifyListeners();
   }
 
